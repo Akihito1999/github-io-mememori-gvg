eada21d6ea1c43ce21192d649eca1509fb212be1
Akihito1999 <tenjobsasa3572@gmail.com>
2025-08-10 12:31:00 +0900

    リポジトリ名設定

--- memento-mori-app/frontend/vite.config.ts
+++ memento-mori-app/frontend/vite.config.ts
@@ -4,5 +4,5 @@ import vue from '@vitejs/plugin-vue'
 // Replace USER/REPO below: base should be "/REPO/" when deployed to Pages
 export default defineConfig({
   plugins: [vue()],
-  base: process.env.GITHUB_PAGES ? '/<REPO_NAME>/' : '/',
+  base: process.env.GITHUB_PAGES ? '/memento-mori-app/' : '/',
 })
